6dacaef898dd32cf4c26eba1c97acbdf13be1d68
Jawid Noori <Hajikaka4030@gmail.com>
2022-09-08 23:02:13 -0700

    config setting updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fantinder', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/fantinder', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
